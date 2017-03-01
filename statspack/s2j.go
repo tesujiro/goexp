94716628de3e5a8185e96f47c299a239edfd8092
tesujiro <tesujiro@gmail.com>
2017-03-01 15:37:53 +0900

    add parsing table header

--- statspack/s2j.go
+++ statspack/s2j.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"strings"
+	"sync"
 )
 
 type Section struct {
@@ -29,8 +31,8 @@ func read(in io.Reader) <-chan string {
 	go func() {
 		input := bufio.NewScanner(in)
 		for input.Scan() {
-			out <- input.Text()
-			//out <- strings.Replace(input.Text(), "\r", "", -1)
+			//out <- input.Text()
+			out <- strings.Replace(input.Text(), "\r", "", -1)
 		}
 		close(out)
 	}()
@@ -45,12 +47,6 @@ var ss_regexp = regexp.MustCompile("([^~]+|^$)")
 func isSubSectionSeparator(s string) bool {
 	return !ss_regexp.MatchString(s)
 	//return ss_regexp.MatchString(s)
-}
-
-var tab_regexp = regexp.MustCompile("[^ -]+")
-
-func isTableSeparator(s string) bool {
-	return !tab_regexp.MatchString(s) && s != ""
 }
 
 func separateSection(in <-chan string) <-chan KeyValue {
@@ -69,7 +65,7 @@ func separateSection(in <-chan string) <-chan KeyValue {
 				}
 				kv.Value += s + "\n" //ToDo: performance
 				if isSubSectionSeparator(s) {
-					fmt.Println("[" + s + "]")
+					//fmt.Println("[" + s + "]")
 				}
 			}
 		}
@@ -79,16 +75,90 @@ func separateSection(in <-chan string) <-chan KeyValue {
 	return out
 }
 
+var tab_regexp = regexp.MustCompile(`[^~ -]+|^[^~-]|^$`)
+var column_regexp = regexp.MustCompile("[-]+|[~]+")
+var spaceline_regexp = regexp.MustCompile("^[ \t]*$")
+var multispace_regexp = regexp.MustCompile("[ \t]+")
+
+func isTableSeparator(s string) bool {
+	return !tab_regexp.MatchString(s)
+}
+
+func getColNames(s string, ci [][]int) []string {
+	//merge each columns
+	var out []string
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i, v := range ci {
+			//fmt.Println(line)
+			//fmt.Println(v)
+			var word string
+			if len(line) >= v[1] {
+				word = line[v[0]:v[1]]
+			} else if len(line) >= v[0] {
+				word = line[v[0]:len(line)]
+			}
+			//fmt.Println(" ==> Too Short")
+			if len(out) <= i {
+				out = append(out, word)
+			} else {
+				out[i] += " " + word
+			}
+		}
+	}
+	//truncate spaces
+	for i, v := range out {
+		out[i] = multispace_regexp.ReplaceAllString(v, " ")
+	}
+	return out
+}
+
+func parseSection(in <-chan KeyValue) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for kv := range in {
+			fmt.Println("=====")
+			fmt.Println(kv.Key)
+			fmt.Println("=====")
+			scanner := bufio.NewScanner(strings.NewReader(kv.Value))
+			var header_lines string
+			for scanner.Scan() {
+				//fmt.Println(scanner.Text())
+				line := scanner.Text()
+				//fmt.Println("[" + line + "]")
+				if spaceline_regexp.MatchString(line) {
+					header_lines = ""
+				} else if isTableSeparator(line) {
+					fmt.Println("Table Separator:")
+					fmt.Println(line)
+					fmt.Println("Header Lines:")
+					fmt.Println(header_lines)
+					ci := column_regexp.FindAllStringIndex(line, -1)
+					//fmt.Println(ci)
+					fmt.Println(getColNames(header_lines, ci))
+					fmt.Println("=====")
+				} else {
+					header_lines += line + "\n"
+				}
+			}
+		}
+	}()
+	wg.Wait()
+}
+
 func main() {
 	/*
-		for s := range read(os.Stdin) {
-			fmt.Println(s)
+		for kv := range separateSection(read(os.Stdin)) {
+			fmt.Println("=====================")
+			fmt.Println(kv.Key)
+			fmt.Println("=====================")
+			//fmt.Println(kv.Value)
 		}
 	*/
-	for kv := range separateSection(read(os.Stdin)) {
-		fmt.Println("=====================")
-		fmt.Println(kv.Key)
-		fmt.Println("=====================")
-		//fmt.Println(kv.Value)
-	}
+	//s := []string{"aaa", "bbb", "ccc"}
+	//fmt.Println(s)
+	parseSection(separateSection(read(os.Stdin)))
 }
