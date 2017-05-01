e162505789dc39afae66c2d17cca64f503f8ffe5
tesujiro <tesujiro@gmail.com>
2017-05-02 05:13:59 +0900

    debug

--- stats_interval.go
+++ stats_interval.go
@@ -11,7 +11,8 @@ import (
 	"time"
 )
 
-const interval float64 = 5 // 5 sec.
+const interval float64 = 1 // 5 sec.
+//const interval float64 = 5 // 5 sec.
 
 type connTable struct {
 	start    map[string]float64
@@ -30,17 +31,18 @@ var token_regex = regexp.MustCompile(`[\s\t\r]+`)
 func (ct *connTable) getLine(line string) {
 	token := token_regex.Split(line, -1)
 	timestamp, _ := strconv.ParseFloat(token[0], 64)
-	src := token[2]
+	src := token[4]
 	flags := token[6]
-	fmt.Printf("timestamp=%f src=%s flags=%s\n", timestamp, src, flags)
+	//fmt.Printf("timestamp=%f src=%s flags=%s\n", timestamp, src, flags)
 	switch {
 	case strings.Contains(flags, "S"):
-		fmt.Println("SYN")
+		//fmt.Println("SYN")
 		ct.start[src] = timestamp
 	case strings.Contains(flags, "F"):
-		fmt.Println("FIN")
-		if _, ok := ct.start[src]; ok {
-			ct.duration[src] = float64(time.Now().UnixNano()/1e9) - timestamp
+		//fmt.Println("FIN")
+		if start, ok := ct.start[src]; ok {
+			//ct.duration[src] = float64(time.Now().UnixNano()/1e9) - timestamp
+			ct.duration[src] = timestamp - start
 		}
 	}
 
@@ -88,7 +90,7 @@ func main() {
 	line := make(chan string, 1)
 	go readLine(os.Stdin, line)
 
-	tick := time.NewTicker(time.Second * 5).C //ToDo:support arg
+	tick := time.NewTicker(time.Second * time.Duration(interval)).C //ToDo:support arg
 
 	ct := newConnTable()
 
@@ -99,7 +101,7 @@ func main() {
 			//fmt.Println("Scanned one line.")
 		case <-tick:
 			ct.report(interval)
-			fmt.Printf("[%s] Time has come!!\n", time.Now().Format("2006/01/02 15:04:05.000 MST"))
+			//fmt.Printf("[%s] Time has come!!\n", time.Now().Format("2006/01/02 15:04:05.000 MST"))
 		}
 	}
 }
