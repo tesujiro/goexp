21f1c464a650a4d4584c0a660e1d5df16af79930
tesujiro <tesujiro@gmail.com>
2017-05-05 07:11:15 +0900

    refactoring

--- stats_interval.go
+++ stats_interval.go
@@ -15,14 +15,16 @@ const interval float64 = 1 // 5 sec.
 //const interval float64 = 5 // 5 sec.
 
 type connTable struct {
-	start    map[string]float64
-	duration map[string]float64
+	start         map[string]float64
+	finish        map[string]float64
+	over_interval map[string]bool
 }
 
 func newConnTable() *connTable {
 	return &connTable{
-		start:    make(map[string]float64),
-		duration: make(map[string]float64),
+		start:         make(map[string]float64),
+		finish:        make(map[string]float64),
+		over_interval: make(map[string]bool),
 	}
 }
 
@@ -41,41 +43,36 @@ func (ct *connTable) getLine(line string) {
 	case strings.Contains(flags, "F"):
 		//fmt.Println("FIN")
 		if start, ok := ct.start[src]; ok {
-			//ct.duration[src] = float64(time.Now().UnixNano()/1e9) - timestamp
-			ct.duration[src] = timestamp - start
+			ct.finish[src] = timestamp - start
+			delete(ct.start, src)
 		}
 	}
-
 }
 
-func (ct *connTable) report(interval float64) {
+func (ct *connTable) report() {
 	var total_time float64
 	var total_count, start_count, finish_count, current_conn int
 
 	now := time.Now()
 	unixNow := float64(now.UnixNano()) / float64(1e9)
 
-	total_count = len(ct.start)
+	total_count = len(ct.start) + len(ct.finish)
+	start_count = total_count - len(ct.over_interval)
+	finish_count = len(ct.finish)
+	current_conn = len(ct.start)
 	for k, start := range ct.start {
-		if start > unixNow-interval {
-			start_count += 1
-		}
-		if duration, ok := ct.duration[k]; ok { // Already Finished.
-			total_time += duration
-			finish_count += 1
-			delete(ct.start, k)
-			delete(ct.duration, k)
-		} else { // Not finished yet.
-			current_conn += 1
-			if start > unixNow-interval {
-				total_time += unixNow - start
-			} else {
-				total_time += interval
-			}
+		total_time += unixNow - start
+		ct.over_interval[k] = true
+	}
+	for k, duration := range ct.finish {
+		total_time += duration
+		delete(ct.finish, k)
+		if _, ok := ct.over_interval[k]; ok {
+			delete(ct.over_interval, k)
 		}
 	}
 
-	fmt.Printf("%s\ttotal=%f\tcount=%d\tstart=%d\tfinish=%d\tcurrent=%d\n",
+	fmt.Printf("[%s]\ttotal=%f\tcount=%d\tstart=%d\tfinish=%d\tcurrent=%d\n",
 		now.Format("2006/01/02 15:04:05.000 MST"),
 		total_time, total_count, start_count, finish_count, current_conn)
 }
@@ -101,7 +98,7 @@ func main() {
 			ct.getLine(l)
 			//fmt.Println("Scanned one line.")
 		case <-tick:
-			ct.report(interval)
+			ct.report()
 			//fmt.Printf("[%s] Time has come!!\n", time.Now().Format("2006/01/02 15:04:05.000 MST"))
 		}
 	}
