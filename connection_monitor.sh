18e547c93be6d3f52c4c08f5a7472a43c62b0e9b
tesujiro <tesujiro@gmail.com>
2017-05-06 05:21:05 +0900

    add interval parameter

--- connection_monitor.sh
+++ connection_monitor.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+if [ $# -ne 2 ];then
+	echo PARAMETER ERROR
+	exit 1
+fi
+
+PORT=$1
+INTERVAL=$2
 IP_ADDR=`ifconfig eth0|grep "inet "|awk '{gsub(/.*:/,"",$2);print $2;}'`
-PORT=80
 
 #echo IP_ADDR=$IP_ADDR
 #exit
 
 dump() {
-      sudo tcpdump -l -tt "src $IP_ADDR and src port $PORT and \
-	(  tcp[tcpflags] & (tcp-syn|tcp-ack)==(tcp-syn|tcp-ack) \
-	or tcp[tcpflags] & (tcp-fin|tcp-ack)==(tcp-fin|tcp-ack) )"
+	sudo tcpdump -l -tt "src $IP_ADDR and src port $PORT and \
+		(  tcp[tcpflags] & (tcp-syn|tcp-ack)==(tcp-syn|tcp-ack) \
+		or tcp[tcpflags] & (tcp-fin|tcp-ack)==(tcp-fin|tcp-ack) )"
 }
 
 
-dump|./stats_interval
+dump|./connection_monitor -interval $INTERVAL
 
