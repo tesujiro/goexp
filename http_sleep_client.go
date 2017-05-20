2afdeb6bd2cf43234f4551ac2c1f5d6c12bf20ef
tesujiro <tesujiro@gmail.com>
2017-05-21 06:00:19 +0900

    refactoring: add context

--- http_sleep_client.go
+++ http_sleep_client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -13,16 +14,17 @@ import (
 )
 
 func main() {
-	err, thread, tester := config()
+	err, threads, tester := config()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	ctx := context.Background()
 	wg := &sync.WaitGroup{}
-	for i := 0; i < thread; i++ {
+	for i := 0; i < threads; i++ {
 		wg.Add(1)
 		go func() {
-			tester.run()
+			tester.run(ctx)
 			wg.Done()
 		}()
 	}
@@ -46,7 +48,7 @@ func config() (error, int, *tester) {
 	t := tester{
 		client: http.DefaultClient,
 	}
-	thread := flag.Int("thread", 10, "threads")
+	threads := flag.Int("thread", 10, "threads")
 	flag.StringVar(&t.url, "url", "http://127.0.0.1:80", "request url")
 	flag.IntVar(&t.loop, "loop", 0, "loop")
 	flag.IntVar(&t.min, "min", 0, "min msec sleep")
@@ -57,17 +59,22 @@ func config() (error, int, *tester) {
 	if t.min > t.max {
 		err := fmt.Errorf("Error: min > max")
 		return err, 0, nil
+	} else if *threads < 0 || t.min < 0 || t.max < 0 || t.loop < 0 {
+		err := fmt.Errorf("Error: negative number")
+		return err, 0, nil
 	}
-	return nil, *thread, &t
+	return nil, *threads, &t
 }
 
-func (t *tester) run() {
+func (t *tester) run(ctx context.Context) {
 	for i := 0; t.loop <= 0 || i < t.loop; i++ {
-		t.get()
+		if err := t.get(); err != nil {
+			log.Println(err)
+		}
 	}
 }
 
-func (t *tester) get() {
+func (t *tester) get() error {
 	values := url.Values{}
 	if t.min == t.max {
 		values.Add("timer", strconv.Itoa(t.min))
@@ -77,32 +84,30 @@ func (t *tester) get() {
 
 	req, err := http.NewRequest("GET", t.url+"/", nil)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	req.URL.RawQuery = values.Encode()
 
 	resp, err := t.client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	defer resp.Body.Close()
 
 	if t.keepalive || t.debug {
 		t.dump(resp)
-		//fmt.Println("Finished!!")
 	}
+	return nil
 }
 
 func (t *tester) dump(resp *http.Response) {
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 	if t.debug {
-		fmt.Print(string(b))
+		log.Print(string(b))
 	}
 }
