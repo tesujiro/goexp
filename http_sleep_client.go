c5a15cb1d0194cba4272eb67fe4c676fce557c98
tesujiro <tesujiro@gmail.com>
2017-05-21 07:18:32 +0900

    add counter and signal

--- http_sleep_client.go
+++ http_sleep_client.go
@@ -9,8 +9,11 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
 	"strconv"
 	"sync"
+	"syscall"
 )
 
 func main() {
@@ -20,15 +23,49 @@ func main() {
 		return
 	}
 	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	//ctx, timeout := context.WithTimeout(ctx, 3*time.Second)
+	//defer timeout()
 	wg := &sync.WaitGroup{}
+
+	// tester goroutin start
+	got := make(chan struct{}, threads)
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
 		go func() {
-			tester.run(ctx)
+			tester.run(ctx, got)
 			wg.Done()
 		}()
 	}
-	wg.Wait()
+
+	// wait goroutine start
+	done := make(chan bool)
+	go func() {
+		wg.Wait()
+		done <- true
+	}()
+
+	// select all the events
+	var counter int
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+L:
+	for {
+		select {
+		case <-got:
+			counter += 1
+		case <-done:
+			break L
+		case <-ctx.Done():
+			//fmt.Printf("Context done:\n")
+			break L
+		case s := <-sig:
+			fmt.Printf("Got signal:%d\n", s)
+			cancel()
+		}
+	}
+	fmt.Printf("%d Requests returned OK.\n", counter)
+
 }
 
 type tester struct {
@@ -66,15 +103,18 @@ func config() (error, int, *tester) {
 	return nil, *threads, &t
 }
 
-func (t *tester) run(ctx context.Context) {
+func (t *tester) run(ctx context.Context, got chan<- struct{}) {
 	for i := 0; t.loop <= 0 || i < t.loop; i++ {
-		if err := t.get(); err != nil {
+		if err := t.get(); err == nil {
+			got <- struct{}{}
+		} else {
 			log.Println(err)
 		}
 	}
 }
 
 func (t *tester) get() error {
+	// set Query parameter "timer=n"
 	values := url.Values{}
 	if t.min == t.max {
 		values.Add("timer", strconv.Itoa(t.min))
@@ -86,7 +126,6 @@ func (t *tester) get() error {
 	if err != nil {
 		return err
 	}
-
 	req.URL.RawQuery = values.Encode()
 
 	resp, err := t.client.Do(req)
