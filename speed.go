1868bf8d9a7dadcf7859728f25802fbd41ff33b0
tesujiro <tesujiro@gmail.com>
2017-05-28 11:09:05 +0900

    add monitor goroutine

--- speed.go
+++ speed.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -18,33 +19,13 @@ func dprintf(format string, a ...interface{}) {
 	}
 }
 
-func getTty() *os.File {
-	device := "/dev/tty"
-	tty, err := os.Create(device)
-	if err != nil {
-		// Openエラー処理
-		fmt.Printf("File Open Error device:%s error:%v\n", device, err)
-	}
-	return tty
-}
-
 func main() {
 	ctx := context.Background()
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithCancel(ctx)
-	//defer cancel()
 
 	var speed *int = flag.Int("bandwidth", 0, "Bytes Per Sec.")
 	flag.Parse()
 
-	tty := getTty()
-	defer tty.Close()
-
-	//limitedPipe(ctx, os.Stdin, os.Stdout, tty, *speed)
-	go limitedPipe(ctx, cancel, os.Stdin, os.Stdout, tty, *speed)
-	//go monitor(ctx, tty)
-
-	<-ctx.Done()
+	limitedPipe(ctx, os.Stdin, os.Stdout, *speed)
 }
 
 const BUFSIZE = 4096
@@ -69,13 +50,23 @@ func read(in io.Reader, rb chan readbuf) {
 	}
 }
 
-//func limitedPipe(ctx context.Context, in io.Reader, out io.Writer, speed int) {
-func limitedPipe(ctx context.Context, cancel func(), in io.Reader, out io.Writer, tty io.Writer, speed int) {
+func limitedPipe(ctx context.Context, in io.Reader, out io.Writer, speed int) {
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithCancel(ctx)
+
 	rbchan := make(chan readbuf, 1)
 	done := make(chan struct{}, 1)
 	go func() {
 		read(in, rbchan)
 		done <- struct{}{}
+	}()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	mon := newMonitor(ctx, cancel)
+	go func() {
+		mon.run()
+		wg.Done()
 	}()
 
 	tick := time.NewTicker(time.Millisecond * time.Duration(200)).C
@@ -90,16 +81,20 @@ L:
 			out.Write(rb.buf)
 			sk.killTime(readBytes)
 		case <-tick:
-			fmt.Fprintf(tty, "\r\033[K[%s] %dBytes\t@ %dKBps",
+			mon.msg <- fmt.Sprintf("\r\033[K[%s] %dBytes\t@ %dKBps",
 				time.Now().Format("2006/01/02 15:04:05.000 MST"),
 				readBytes,
 				sk.currentSpeed(readBytes)/1024)
 		case <-done:
-			fmt.Fprintf(tty, "\n")
+			mon.msg <- fmt.Sprintf("\n")
+			cancel()
+			break L
+		case <-ctx.Done():
 			break L
 		}
 	}
-	cancel()
+	//<-ctx.Done()
+	wg.Wait()
 }
 
 type speedKeeper struct {
@@ -137,4 +132,48 @@ func (sk *speedKeeper) currentSpeed(curBytes int) int {
 		return 0
 	}
 	return curBytes * 1e9 / d
+}
+
+type monitor struct {
+	ctx    context.Context
+	cancel func()
+	tty    io.Writer
+	//tty *os.File
+	msg chan string
+}
+
+func getTty() *os.File {
+	device := "/dev/tty"
+	tty, err := os.Create(device)
+	if err != nil {
+		// Openエラー処理
+		fmt.Printf("File Open Error device:%s error:%v\n", device, err)
+	}
+	return tty
+}
+
+func newMonitor(ctx context.Context, cancel func()) *monitor {
+	tty := getTty()
+	return &monitor{
+		ctx:    ctx,
+		cancel: cancel,
+		tty:    tty,
+		msg:    make(chan string),
+	}
+}
+
+func (mon *monitor) print(s string) {
+	fmt.Fprint(mon.tty, s)
+}
+
+func (mon *monitor) run() {
+L:
+	for {
+		select {
+		case m := <-mon.msg:
+			mon.print(m)
+		case <-mon.ctx.Done():
+			break L
+		}
+	}
 }
