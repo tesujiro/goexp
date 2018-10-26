cc42d420ea5c782f2cbb6adb57df0a6ec2af61de
tesujiro <tesujiro@gmail.com>
2018-10-26 19:11:21 +0900

    add: lock campaigns while appending a campaign

--- promotion/banner.go
+++ promotion/banner.go
@@ -2,7 +2,9 @@ package promotion
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -14,9 +16,51 @@ type Campaign struct {
 }
 
 var campaigns []Campaign
+var mu *sync.Mutex
 
 func init() {
 	campaigns = make([]Campaign, 0)
+	mu = new(sync.Mutex)
+}
+
+var ipAddrList = []net.IP{
+	net.IPv4(10, 0, 0, 1),
+	net.IPv4(10, 0, 0, 2),
+}
+
+func isAdminIP(ip net.IP) bool {
+	for i := 0; i < len(ipAddrList); i++ {
+		if ipAddrList[i].Equal(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+// FromRequest extracts the user IP address from req, if present.
+// https://blog.golang.org/context/userip/userip.go
+func getIPFromRequest(req *http.Request) (net.IP, error) {
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
+	}
+	return userIP, nil
+}
+
+func isFromAdmin(req *http.Request) (bool, error) {
+	ip, err := getIPFromRequest(req)
+	if err != nil {
+		return false, err
+	}
+	if isAdminIP(ip) {
+		return true, nil
+	}
+	return false, nil
 }
 
 /*
@@ -42,7 +86,10 @@ func (c *Campaign) Add() error {
 		return fmt.Errorf("error: banner is not set.")
 	}
 
+	mu.Lock()
 	campaigns = append(campaigns, *c)
+	mu.Unlock()
+
 	return nil
 }
 
@@ -50,10 +97,12 @@ func (c *Campaign) duringThePeriod(t time.Time) bool {
 	return (c.startAt.Before(t) || c.startAt.Equal(t)) && (c.expiresAt.After(t) || c.expiresAt.Equal(t))
 }
 
+// truncate campaigns for testing
 func truncate() {
 	campaigns = make([]Campaign, 0)
 }
 
+/*
 func list() []Campaign {
 	cs := make([]Campaign, 0)
 	for _, c := range campaigns {
@@ -62,15 +111,29 @@ func list() []Campaign {
 	}
 	return cs
 }
+*/
+
+// number of campaigns for testing
+func countCampaigns() int {
+	return len(campaigns)
+}
 
 var now = time.Now
 
 func Banner(r *http.Request) (string, error) {
 	var banner string
 	var expiresAt time.Time
+
+	// Check ip address
+	isAdmin, err := isFromAdmin(r)
+	if err != nil {
+		return "", fmt.Errorf("cannot check address:%v", err)
+	}
+
+	// Check periods of campaigns
 	n := now()
 	for _, c := range campaigns {
-		if c.duringThePeriod(n) {
+		if isAdmin || c.duringThePeriod(n) {
 			if expiresAt.IsZero() || c.expiresAt.Before(expiresAt) {
 				expiresAt = c.expiresAt
 				banner = c.banner
--- promotion/banner_test.go
+++ promotion/banner_test.go
@@ -4,11 +4,42 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestCampaignPeriod(t *testing.T) {
+func TestConcurrentAddCampaign(t *testing.T) {
+	conc := 10000 // Concurrency
+	camp_fr := time.Date(2018, time.October, 01, 0, 0, 0, 0, time.UTC)
+	camp_to := time.Date(2018, time.October, 14, 0, 0, 0, 0, time.UTC)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < conc; i++ {
+		wg.Add(1)
+		go func() {
+			err := NewCampaign(
+				fmt.Sprintf("Camp%v", i),
+				camp_fr,
+				camp_to,
+				fmt.Sprintf("Camp%v", i),
+			).Add()
+			if err != nil {
+				t.Fatalf("add campaign failed: %v", err)
+			}
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	count := countCampaigns()
+	if count != conc {
+		t.Errorf("campaigns number:%v - expected: %v ", count, conc)
+	}
+	truncate()
+}
+
+func setCampaignsCase1(t *testing.T) {
 	loc_tokyo, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		t.Fatalf("LoadLocation loc1 failed:%v\n", err)
@@ -43,41 +74,73 @@ func TestCampaignPeriod(t *testing.T) {
 		}
 	}
 
-	req_local, err := http.NewRequest("GET", "dummy", nil)
+}
+
+func TestCampaignPeriod(t *testing.T) {
+	// set campaigns case 1
+	setCampaignsCase1(t)
+
+	loc_tokyo, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		t.Fatalf("LoadLocation loc1 failed:%v\n", err)
+	}
+	loc_newyork, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		t.Fatalf("LoadLocation loc1 failed:%v\n", err)
+	}
+
+	// request from user
+	ReqFromUser, err := http.NewRequest("GET", "DUMMY URL", nil)
 	if err != nil {
 		fmt.Printf("NewRequest error:%v\n", err)
 	}
+	ReqFromUser.RemoteAddr = "127.0.0.1:12345"
+
+	// request from admin1
+	ReqFromAdmin1, err := http.NewRequest("GET", "DUMMY URL", nil)
+	if err != nil {
+		fmt.Printf("NewRequest error:%v\n", err)
+	}
+	ReqFromAdmin1.RemoteAddr = "10.0.0.1:12345"
+
+	// request from admin2
+	ReqFromAdmin2, err := http.NewRequest("GET", "DUMMY URL", nil)
+	if err != nil {
+		fmt.Printf("NewRequest error:%v\n", err)
+	}
+	ReqFromAdmin2.RemoteAddr = "10.0.0.2:12345"
+
 	tests := []struct {
 		now     time.Time
 		request *http.Request
 		banner  string
 	}{
 		// before campaigns
-		{now: time.Date(2018, time.September, 30, 23, 59, 59, 0, loc_tokyo), request: req_local, banner: ""},
-		{now: time.Date(2018, time.September, 30, 10, 59, 59, 0, loc_newyork), request: req_local, banner: ""},
+		{now: time.Date(2018, time.September, 30, 23, 59, 59, 0, loc_tokyo), request: ReqFromUser, banner: ""},
+		{now: time.Date(2018, time.September, 30, 10, 59, 59, 0, loc_newyork), request: ReqFromUser, banner: ""},
 		// campaign 1 started
-		{now: time.Date(2018, time.October, 01, 00, 00, 0, 0, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
-		{now: time.Date(2018, time.September, 30, 11, 00, 0, 0, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 01, 00, 00, 0, 0, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.September, 30, 11, 00, 0, 0, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
 		// before campaign 2 start
-		{now: time.Date(2018, time.October, 07, 21, 59, 59, 999, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
-		{now: time.Date(2018, time.October, 07, 8, 59, 59, 999, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 07, 21, 59, 59, 999, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 07, 8, 59, 59, 999, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
 		// campaign 2 start
-		{now: time.Date(2018, time.October, 07, 22, 00, 0, 0, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
-		{now: time.Date(2018, time.October, 07, 9, 00, 0, 0, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 07, 22, 00, 0, 0, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 07, 9, 00, 0, 0, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
 		// campaign 1 last moment
-		{now: time.Date(2018, time.October, 14, 00, 00, 0, 0, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
-		{now: time.Date(2018, time.October, 13, 11, 00, 0, 0, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 14, 00, 00, 0, 0, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
+		{now: time.Date(2018, time.October, 13, 11, 00, 0, 0, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 1</some>"},
 		// campaign 1 expired
-		{now: time.Date(2018, time.October, 14, 00, 00, 0, 1, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 2</some>"},
-		{now: time.Date(2018, time.October, 13, 11, 00, 0, 1, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 2</some>"},
+		{now: time.Date(2018, time.October, 14, 00, 00, 0, 1, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 2</some>"},
+		{now: time.Date(2018, time.October, 13, 11, 00, 0, 1, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 2</some>"},
 		// before campaign 2 expires
-		{now: time.Date(2018, time.October, 22, 07, 00, 0, 0, loc_tokyo), request: req_local, banner: "<some>CAMPAIGN 2</some>"},
-		{now: time.Date(2018, time.October, 21, 18, 00, 0, 0, loc_newyork), request: req_local, banner: "<some>CAMPAIGN 2</some>"},
+		{now: time.Date(2018, time.October, 22, 07, 00, 0, 0, loc_tokyo), request: ReqFromUser, banner: "<some>CAMPAIGN 2</some>"},
+		{now: time.Date(2018, time.October, 21, 18, 00, 0, 0, loc_newyork), request: ReqFromUser, banner: "<some>CAMPAIGN 2</some>"},
 		// after campaigns
-		{now: time.Date(2018, time.October, 22, 07, 00, 0, 1, loc_tokyo), request: req_local, banner: ""},
-		{now: time.Date(2018, time.October, 21, 18, 00, 0, 1, loc_newyork), request: req_local, banner: ""},
+		{now: time.Date(2018, time.October, 22, 07, 00, 0, 1, loc_tokyo), request: ReqFromUser, banner: ""},
+		{now: time.Date(2018, time.October, 21, 18, 00, 0, 1, loc_newyork), request: ReqFromUser, banner: ""},
+		// request from administrator
 	}
-	//list()
 
 	for i, c := range tests {
 		t.Logf("test case:%v\tnow:%v", i, c.now)
@@ -91,7 +154,25 @@ func TestCampaignPeriod(t *testing.T) {
 			t.Errorf("case:%v received: %v - expected: %v - case: %v", i, banner, c.banner, c)
 		}
 	}
+	truncate()
 }
+
+/*
+func TestIpAddress(t *testing.T) {
+	// set campaigns case 1
+	setCampaignsCase1(t)
+
+	loc_tokyo, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		t.Fatalf("LoadLocation loc1 failed:%v\n", err)
+	}
+	loc_newyork, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		t.Fatalf("LoadLocation loc1 failed:%v\n", err)
+	}
+
+}
+*/
 
 func BenchmarkBanner(b *testing.B) {
 	benchBanner(b, 100)
@@ -116,14 +197,14 @@ func benchBanner(b *testing.B, camps int) {
 				b.Fatalf("add campaign failed: %v", err)
 			}
 		}
-		req_local, err := http.NewRequest("GET", "dummy", nil)
+		ReqFromUser, err := http.NewRequest("GET", "dummy", nil)
 		if err != nil {
 			b.Fatalf("NewRequest error:%v\n", err)
 		}
 		now = func() time.Time { return randTime() }
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			banner, err := Banner(req_local)
+			banner, err := Banner(ReqFromUser)
 			if err != nil {
 				b.Fatalf("Banner func failed:%v\n", err)
 			}
