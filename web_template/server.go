8fc1f36fa37b821cd43e7ba4863ab7a6c3af07df
tesujiro <tesujiro@gmail.com>
2018-12-05 07:30:23 +0900

    add: redirect

--- web_template/server.go
+++ web_template/server.go
@@ -22,6 +22,7 @@ func main() {
 }
 
 func (s *server) routes() {
+	s.router.HandleFunc("/", s.handleDefault())
 	s.router.HandleFunc("/greet", s.handleHello())
 	s.router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
 }
@@ -29,5 +30,11 @@ func (s *server) routes() {
 func (s *server) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World")
+	}
+}
+
+func (s *server) handleDefault() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/static", 301)
 	}
 }
