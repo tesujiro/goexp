5d4f848e90465074b5ebc8400c31899cc1abcf29
tesujiro <tesujiro@gmail.com>
2018-12-16 06:19:03 +0900

    refact: changed node.left node.right to node.child[2]

--- trie/btrie.go
+++ trie/btrie.go
@@ -27,9 +27,10 @@ func init() {
 type node struct {
 	x      number
 	parent *node
-	left   *node
-	right  *node
-	jump   *node
+	//left   *node
+	//right  *node
+	child [2]*node
+	jump  *node
 }
 
 //func newNode() *node {
@@ -44,8 +45,8 @@ type binaryTrie struct {
 
 func newBinaryTrie() *binaryTrie {
 	dummy := &node{}
-	dummy.left = dummy
-	dummy.right = dummy
+	dummy.child[0] = dummy
+	dummy.child[1] = dummy
 
 	return &binaryTrie{
 		root:  &node{jump: dummy},
@@ -55,10 +56,10 @@ func newBinaryTrie() *binaryTrie {
 }
 
 func (bt *binaryTrie) GetAll() (result []number) {
-	w := bt.dummy.right
+	w := bt.dummy.child[1]
 	for w != nil && w != bt.dummy {
 		result = append(result, w.x)
-		w = w.right
+		w = w.child[1]
 	}
 	return
 }
@@ -74,76 +75,44 @@ func (bt *binaryTrie) Add(x number) bool {
 	// 1 - search for x until following out oft trie
 	for i = 0; i < bt.w; i++ {
 		c = uint(x) >> (bt.w - i - 1) & 1
-		if u == nil {
+		if u == nil || u.child[c] == nil {
 			break
 		}
-		if c == 0 {
-			if u.left == nil {
-				break
-			}
-			u = u.left
-		} else {
-			if u.right == nil {
-				break
-			}
-			u = u.right
-		}
+		u = u.child[c]
 	}
 	if i == bt.w {
 		// bt already has x
 		return false
 	}
 	var pred *node
-	if c == 1 { //right
-		pred = u.jump
+	if c == 0 { //right
+		pred = u.jump.child[c]
 	} else { //left
-		pred = u.jump.left
+		pred = u.jump
 	}
-	/*
-		if pred == nil {
-			pred = bt.dummy
-		}
-	*/
 	u.jump = nil
 
 	// 2 - add path to x
 	for ; i < bt.w; i++ {
 		c = uint(x) >> (bt.w - i - 1) & 1
-		if c == 0 {
-			u.left = &node{}
-			u.left.parent = u
-			u = u.left
-		} else {
-			u.right = &node{}
-			u.right.parent = u
-			u = u.right
-		}
-		/*
-			var n **node
-			if c == 0 {
-				n = &u.left
-			} else {
-				n = &u.right
-			}
-			*n = &node{}
-			(*n).parent = u
-			u = *n
-		*/
+		u.child[c] = &node{}
+		u.child[c].parent = u
+		u = u.child[c]
 	}
 	u.x = x
 
 	// 3 = add u to linked list
-	u.left = pred
-	u.right = pred.right
-	u.left.right = u
-	u.right.left = u
-	//fmt.Printf("%v => %v => %v\n", u.left.x, u.x, u.right.x)
+	u.child[0] = pred
+	u.child[1] = pred.child[1]
+	u.child[0].child[1] = u
+	u.child[1].child[0] = u
+	//fmt.Printf("%v => %v => %v\n", u.child[0].x, u.x, u.child[1].x)
 
 	// 4 - walk back up, updating jump pointers
 	v := u.parent
 	for v != nil {
-		if (v.left == nil && (v.jump == nil || v.jump.x > x)) ||
-			(v.right == nil && (v.jump == nil || v.jump.x < x)) {
+		if (v.child[0] == nil && (v.jump == nil || v.jump.x > x)) ||
+			(v.child[1] == nil && (v.jump == nil || v.jump.x < x)) {
 			v.jump = u
 		}
 		v = v.parent
@@ -151,6 +120,26 @@ func (bt *binaryTrie) Add(x number) bool {
 
 	return true
 }
+
+/*
+func (e1 number) Compare(e2 ch01.Comparable) int {
+	return int(e1 - e2.(number))
+}
+
+func (e1 number) HashCode() uint {
+	return uint(e1)
+}
+
+func testCh13() {
+	s := ch13.NewBinaryTrie()
+	for _, v := range table {
+		b := s.Add(v)
+		_ = b
+		//fmt.Printf("Add(%v)=>%v\n", v, b)
+	}
+	s.Print()
+}
+*/
 
 func main() {
 	bt := newBinaryTrie()
@@ -160,4 +149,5 @@ func main() {
 		//fmt.Printf("Add(%v)=>%v\n", v, b)
 	}
 	bt.Print()
+
 }
