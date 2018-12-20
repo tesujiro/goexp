d83707a4442f5c34350b1fd20f43a129c1b35b64
tesujiro <tesujiro@gmail.com>
2018-12-20 21:04:22 +0900

    removed duplicate hash update and debug print

--- trie/xftrie.go
+++ trie/xftrie.go
@@ -51,13 +51,13 @@ func (bt *xFastTrie) Add(x number) bool {
 		// if not found set pred
 		if exist && u.child[c] == nil {
 			exist = false
-			if c == 0 { //right
+			if c == 0 { //left
 				pred = u.jump.child[0]
-			} else { //left
+			} else { //right
 				pred = u.jump
 			}
 
-			u.jump = nil //buggy?
+			u.jump = nil
 		}
 
 		// 2 - if not found add path to x
@@ -66,35 +66,28 @@ func (bt *xFastTrie) Add(x number) bool {
 			u.child[c].parent = u
 			val := number(uint(x) >> (bt.w - i - 1))
 			bt.t[i+1][val] = u.child[c]
-			debugf("set t[%v][%v]\n", i+1, val)
 		}
 		u = u.child[c]
-		//u.x = number(uint(x) >> (bt.w - i)) //debug
-		//debugf("i=%v ", i)
 	}
 	if exist {
 		return false
 	}
-	//debugf(" ==> set u.x=%v\n", x)
 	u.x = x
-	bt.t[bt.w][x] = u
+	//bt.t[bt.w][x] = u
 
 	// 3 = add u to linked list
 	u.child[0] = pred
 	u.child[1] = pred.child[1]
 	u.child[0].child[1] = u
 	u.child[1].child[0] = u
-	//debugf("%v => %v => %v\n", u.child[0].x, u.x, u.child[1].x)
 
 	// 4 - walk back up, updating jump pointers
 	for v := u.parent; v != nil; v = v.parent {
 		if (v.child[0] == nil && (v.jump == nil || v.jump.x > x)) ||
 			(v.child[1] == nil && (v.jump == nil || v.jump.x < x)) {
-			//debugf("set jump to%v\n", u.x)
 			v.jump = u
 		} else {
 			if v.jump == nil {
-				debugf("NOT SET JUMP %v !!!\n", u.x)
 			}
 		}
 	}
@@ -112,30 +105,17 @@ func (bt *xFastTrie) Find(x number) number {
 		p := x >> (bt.w - i)
 		v, ok := bt.t[i][p]
 		if !ok {
-			debugf("N(i:%v,u.x:%v) ", i, u.x)
 			h = i
 		} else {
-			debugf("Y(i:%v,u.x:%v->%v) ", i, u.x, v.x)
 			u = v
 			l = i
 		}
 		loop++
 	}
-	debugf("\n")
-	//debugf("loop=%v \n", loop)
 	// found x
 	if l == bt.w {
-		//debugf("found u.x=%v\n", int(u.x))
-		debugf("loop=%v FOUND %v\n", loop, x)
 		return u.x
 	}
-
-	/*
-		if u.jump == nil {
-			debugf("loop=%v not found %v u.jump==nil u.x=%v u.parent.x=%v %v-%v\n", loop, x, u.x, u.parent.x, l, h)
-			return 0
-		}
-	*/
 
 	// search for next value
 	c := x >> (bt.w - l - 1) & 1
@@ -143,16 +123,11 @@ func (bt *xFastTrie) Find(x number) number {
 	if c == 1 {
 		pred = u.jump
 	} else {
-		//debugf("u=%#v\n", u)
-		//debugf("u.jump=%#v\n", u.jump)
 		pred = u.jump.child[0]
 	}
-	//if pred.right() == bt.dummy || pred.right() == nil {
 	if pred.child[1] == bt.dummy {
-		debugf("loop=%v, not found %v, return 0\n", loop, x)
 		return 0
 	} else {
-		debugf("loop=%v, not found %v, return next %v, next?:%v\n", loop, x, pred.child[1].x, x < pred.child[1].x)
 		return pred.child[1].x
 	}
 }
