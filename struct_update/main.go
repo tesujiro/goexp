39bd738d7d1fd76a4d3fbf7bf0033c24a484b809
tesujiro <tesujiro@gmail.com>
2019-03-24 21:20:16 +0900

    add: better implementation

--- struct_update/main.go
+++ struct_update/main.go
@@ -20,16 +20,30 @@ func f2(p *Parent) {
 	*p = Child{number: 3}
 }
 
+func f3(p Parent) {
+	c := p.(*Child)
+	fmt.Printf("c=%#v\n", c)
+	*c = Child{number: 3}
+}
+
 func main() {
 	c1 := Child{number: 1}
 	//c2 := Child{number: 2}
 
+	// BAD
 	fmt.Printf("c1=%#v\n", c1)
 	f1(c1)
 	fmt.Printf("c1=%#v\n", c1)
 
+	// NOT GOOD
 	c1p := Parent(c1)
 	fmt.Printf("c1p=%#v\n", c1p)
 	f2(&c1p)
 	fmt.Printf("c1p=%#v\n", c1p)
+
+	// GOOD!
+	fmt.Printf("c1=%#v\n", c1)
+	f3(&c1)
+	fmt.Printf("c1=%#v\n", c1)
+
 }
