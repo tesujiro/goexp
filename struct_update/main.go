03fc0189e1c4081af2d96935087b2c1e952efe4f
tesujiro <tesujiro@gmail.com>
2019-03-24 21:09:43 +0900

    add: update struct

---
+++ struct_update/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Parent interface{}
+
+type Child struct {
+	number int
+}
+
+func f1(p Parent) {
+	c := p.(Child)
+	fmt.Printf("c=%#v\n", c)
+	p = Child{number: 3}
+}
+
+func f2(p *Parent) {
+	c := (*p).(Child)
+	fmt.Printf("c=%#v\n", c)
+	*p = Child{number: 3}
+}
+
+func main() {
+	c1 := Child{number: 1}
+	//c2 := Child{number: 2}
+
+	fmt.Printf("c1=%#v\n", c1)
+	f1(c1)
+	fmt.Printf("c1=%#v\n", c1)
+
+	c1p := Parent(c1)
+	fmt.Printf("c1p=%#v\n", c1p)
+	f2(&c1p)
+	fmt.Printf("c1p=%#v\n", c1p)
+}
