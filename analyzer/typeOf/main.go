cffa11499b1b55ce37a0a8983230a945addfe202
tesujiro <tesujiro@gmail.com>
2019-08-05 14:19:14 +0900

    add: TypeOf sample

---
+++ analyzer/typeOf/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"log"
+)
+
+const src = `package main
+func main() {
+v := 100
+println(v + 1)
+sub()
+}
+func sub() {
+v := "abc"
+println(v + "xyz")
+}`
+
+func main() {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "main.go", src, 0)
+	if err != nil {
+		/* ؒٓ٦Ⳣ椚 */
+		log.Fatal(err)
+	}
+	cfg := &types.Config{Importer: importer.Default()}
+	info := &types.Info{
+		Types: map[ast.Expr]types.TypeAndValue{},
+	}
+	_, err = cfg.Check("main", fset, []*ast.File{file}, info)
+	if err != nil {
+		/* ؒٓ٦Ⳣ椚 */
+		log.Fatal(err)
+	}
+	ast.Inspect(file, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.BinaryExpr:
+			pos := fset.Position(n.Pos())
+			typ := info.TypeOf(n)
+			fmt.Printf("type of %v is %v\n", pos, typ)
+		}
+		return true
+	})
+}
