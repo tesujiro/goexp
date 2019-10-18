486ac612d55c1f3da6c4bcdbc6610690428bd2d2
tesujiro <tesujiro@gmail.com>
2019-10-19 06:55:26 +0900

    add: pprof & test

--- length/length.go
+++ length/length.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+
+	"github.com/pkg/profile"
 )
 
 func main() {
-	list := newSearchListWithLimitedFails(129, 256, 3)
-	fmt.Println(list.length())
+	var l int
+	N := 1000000
+	number := make([]int, N)
+	size := make([]int, N)
+	fail := make([]int, N)
+	for i := 0; i < N; i++ {
+		size[i] = rand.Intn(1 << 32)
+		number[i] = rand.Intn(size[i] + 1)
+		fail[i] = rand.Intn(16)
+	}
+	fmt.Println("path1")
+
+	defer profile.Start(profile.ProfilePath(".")).Stop()
+
+	for i := 0; i < N; i++ {
+		list := newBinSearchList(number[i])
+		l = list.length()
+	}
+	fmt.Println("path2")
+	for i := 0; i < N; i++ {
+		list := newBinSearchListWithLimitedSize(number[i], size[i])
+		l = list.length()
+	}
 	/*
-		var l int
-		N := 10000000
-		defer profile.Start(profile.ProfilePath(".")).Stop()
-			for i := 0; i < N; i++ {
-				rnd := rand.Intn(N)
-				s := newBinSearchListWithLimitedSize(rnd, rnd+1)
-				l = s.length()
-			}
-			_ = l
+		fmt.Println("path3")
+		for i := 0; i < N; i++ {
+			list := newSearchListWithLimitedFails(number[i], size[i], fail[i])
+			l = list.length()
+		}
 	*/
+	_ = l
 }
