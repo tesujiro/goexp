316228697b6f457e9de9c166e0d6cbd35f3f54b4
tesujiro <tesujiro@gmail.com>
2020-04-26 10:59:43 +0900

    update

--- replaceWithEscape/main.go
+++ replaceWithEscape/main.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
-	str := "&a\\&"
+	str := "&a&b\\&c"
 	fmt.Println(str)
-	re := regexp.MustCompile(`^\&`)
-	//re := regexp.MustCompile(`^&`)
-	fmt.Println(re.ReplaceAllString(str, "X"))
+	//re := regexp.MustCompile(`([!\\])\&`)
+	re := regexp.MustCompile(`(^|[^\\])\&`)
+	//re := regexp.MustCompile(`([^a])\&`)
+	//re := regexp.MustCompile(`(!a)\&`)
+	//re := regexp.MustCompile(`(a)\&`)
+	//re := regexp.MustCompile(`\&`)
+	//re := regexp.MustCompile(`^[\\\\]\&`)
+	//re := regexp.MustCompile(`^\&`)
+	//re := regexp.MustCompile(`(^[\\\\])&`)
+	//re := regexp.MustCompile(`(^(\\\\))&`)
+	//re := regexp.MustCompile(`(^\\\\)&`)
+	//re := regexp.MustCompile(`\\&`)
+	fmt.Println(re.ReplaceAllString(str, "${1}X"))
+	//fmt.Println(re.ReplaceAllString(str, "X"))
 }
